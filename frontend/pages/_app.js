62ea6b3671886852300fbc93fd97ca975c968356
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-02-20 12:08:56 +0100

    adds authorization

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,29 +1,59 @@
-import React from 'react';
-import App from 'next/app';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
+import Cookie from 'js-cookie';
+import fetch from 'isomorphic-fetch';
 import Layout from '../components/Layout';
+import AppContext from '../context/AppContext';
 import withApollo from '../lib/apollo';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Head>
-          <link
-            rel='stylesheet'
-            href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
-            integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
-            crossOrigin='anonymous'
-          />
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </>
-    );
-  }
-}
+const App = ({ Component, pageProps }) => {
+  const [user, setUser] = useState();
 
-export default withApollo({})(MyApp);
+  useEffect(() => {
+    // grab token value from cookie
+    const token = Cookie.get('token');
 
+    if (token) {
+      // authenticate the token on the server and place set user object
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}users/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then(async (res) => {
+        // if res comes back not valid, token is not valid
+        // delete the token and log the user out on client
+        if (!res.ok) {
+          Cookie.remove('token');
+          setUser(null);
+          return null;
+        }
+        const user = await res.json();
+        setUser(user);
+      });
+    }
+  }, []);
+
+  return (
+    <AppContext.Provider
+    value={{
+      user: user,
+      isAuthenticated: !!user,
+      setUser,
+    }}
+  >
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+          integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
+          crossOrigin='anonymous'
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContext.Provider>
+  );
+};
+
+export default withApollo({})(App);
