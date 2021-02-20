932255a12aa8421d1b4995ea8c1d1cc90f6fb437
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-02-20 10:30:58 +0100

    adds the initial version

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b234be9a-d5e4-44cc-babd-b44088651dd7'
+};
---
+++ frontend/pages/index.js
@@ -0,0 +1,38 @@
+/* pages/index.js */
+import { useState } from 'react';
+import { Button, Alert } from "reactstrap";
+
+import { Col, Input, InputGroup, InputGroupAddon, Row } from "reactstrap";
+import RestaurantList from "../components/RestaurantList";
+
+function Home() {
+  const [query, updateQuery] = useState("");
+  return (
+    <div className="container-fluid">
+      <Row>
+        <Col>
+          <div className="search">
+            <InputGroup>
+              <InputGroupAddon addonType="append"> Search </InputGroupAddon>
+              <Input
+                onChange={e => updateQuery(e.target.value.toLocaleLowerCase())}
+                value={query}
+              />
+            </InputGroup>
+          </div>
+          <RestaurantList search={query} />
+        </Col>
+      </Row>
+      <style jsx>
+        {`
+          .search {
+            margin: 20px;
+            width: 500px;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
+
+export default Home;
